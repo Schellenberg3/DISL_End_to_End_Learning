104fbf90406635da2a1df5240282d15bb6c19e9b
Schellenberg3 <schellenberg.andrew@gmail.com>
2021-02-23 20:10:42 -0500

    add statistic check for incomplete episodes

--- renumber_dataset.py
+++ renumber_dataset.py
@@ -1,5 +1,6 @@
 import os
 import time
+import numpy as np
 
 if __name__ == '__main__':
     broken_dataset_dir = 'datasets/randomized/training/DislPickUpBlueCup/variation0/episodes'
@@ -20,20 +21,38 @@ if __name__ == '__main__':
     if ans not in ['y', 'yes', 'Y', 'Yes']:
         exit(f'[Warn] Answer: {ans} not recognized. Exiting program without altering any files.')
 
+    count = []
+
     start_rename = time.perf_counter()
 
     # All files are given temp names first to avoid conflict
     for i, dir_name in enumerate(dir_to_rename):
         old_name = f'{broken_dataset_dir}/{dir_name}'
+        count.append(len(os.listdir(f'{old_name}/front_rgb')))
         temp_name = f'{broken_dataset_dir}/episode{i}_temp'
         os.renames(old_name, temp_name)
 
+    count = np.asarray(count)
+    avg = np.mean(count)
+    std = np.std(count)
+    factor = 1.25
+    possible_errors = []
+
     for i, dir_name in enumerate(os.listdir(broken_dataset_dir)):
         old_name = f'{broken_dataset_dir}/{dir_name}'
+        size = len(os.listdir(f'{old_name}/front_rgb'))
         new_name = f'{broken_dataset_dir}/episode{i}'
         os.renames(old_name, new_name)
+        if size < (avg - factor*std):
+            possible_errors.append(new_name)
 
     end_rename = time.perf_counter()
 
-    print(f'[Info] Successfully renamed {num_to_rename} files at: {broken_dataset_dir}. '
+    if len(possible_errors) != 0:
+        print(f'\n[WARN] The following files seem to have fewer time steps in then than most '
+              f'(the average was {avg} with standard deviation of +/-{std}). '
+              f'It would be wise to manually check these')
+        [print(possible_error) for possible_error in possible_errors]
+
+    print(f'\n[Info] Successfully renamed {num_to_rename} files at: {broken_dataset_dir}. '
           f'Process took {end_rename - start_rename} seconds. Exiting program.')
